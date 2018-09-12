5bd8c67ce0a4acb1755587809029b5b96307ccf3
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-09-12 18:21:32 -0500

    Student project template for P3

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+//
+// COP3014 - Reach Out and Touch Someone
+
+/*** Don't change any text below */
+// Important announcement on Significance of Academic Honor Policy
+// The  Academic Honor Policy is an important aspect of keeping the integrity
+// and quality of the computer science courses in general. Please don't share
+// your project source with any other students on any occasion. I would like
+// to let you know that, for the summer 2018 COP3014 course, we found several
+// students violated the Academic Honor Policy by sharing their programming
+// assignment source in an advertent and inadvertent manners. Please note that
+// we will run MOSS" (plagiarism detection software tool) on future COP3014 student
+// project submissions together with your summer 2018 project" submissions.
+// In case your project source code happens to be >%75" similar to future student
+// project source (which is statistically" almost impossible), your grade will
+// be invalidated and your academic honor code violation will be reported to Dean's
+// office. The following is the quick reference for Academic Honor Policy:"
+// https://fda.fsu.edu/sites/g/files/upcbnu636/files/Media/Files/Academic%20Honor%20Policy/AHP%20Quick%20Reference%20Guide%20Nov%202017.pdf
+//
+
+/*** Add your comments starting from here ***/
+
+int main() {
+
+    /* The price for a call is calculated as follows:
+       20  cents a minute.
+       50% off if originated on Monday,Tuesday,Wednesday, or Thursday
+           if not between 8am to 5pm.
+       40% off if originated on Friday
+           if not between 8am to noon.
+       75% off if originated on weekends.
+       15% off on any minutes after an hour at the given rate.
+        7% federal tax. */
+    // Add your implementation here!
+
+    return 0;
+}
